c28f5d5bfeac748556908302cb508a0a80446ed1
Christopher O'Connell <christopher.oconnell.webdev@gmail.com>
2025-10-06 00:06:23 -0400

    Added Mobile Responsive Styles

--- js/main.js
+++ js/main.js
@@ -7,6 +7,17 @@ document.addEventListener("DOMContentLoaded", async () => {
   const pages = await loadData();
 
   renderSidebar(document.querySelector("#sidebar-pages"), pages);
+  
+  // Sidebar toggle for mobile
+  const sidebarToggleBtn = document.getElementById("sidebar-toggle");
+  const sidebar = document.getElementById("sidebar");
+  if (sidebarToggleBtn && sidebar) {
+      sidebarToggleBtn.addEventListener("click", () => {
+          sidebar.classList.toggle("expanded");
+      });
+  }
+
+  
   setupSearch(pages);
   setupRouter(pages);
 });
